bd9893068b8071462abdb62aca26504ecc806533
Shubha274 <pandeyshubha27@gmail.com>
2025-07-08 06:53:20 +0530

    changes added

--- server/config/db.js
+++ server/config/db.js
@@ -11,10 +11,13 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(
+      `${process.env.MONGO_URI}/screenographix`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error("MongoDB connection FAILED:", error.message);
