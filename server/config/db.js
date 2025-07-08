7607c1bfce739c2dcd6e0ecdf543b570cb809fc2
Shubha274 <pandeyshubha27@gmail.com>
2025-07-08 06:34:33 +0530

    changes done

--- server/config/db.js
+++ server/config/db.js
@@ -1,12 +1,25 @@
 import mongoose from "mongoose";
+// const connectDB = async () => {
+// try {
+//   mongoose.connection.on("connected", () =>
+//     console.log("Database connected")
+//   );
+//   await mongoose.connect(`${process.env.mongo_url}/screenographix`);
+// } catch (error) {
+//   console.error(error.message);
+// }
+
 const connectDB = async () => {
   try {
-    mongoose.connection.on("connected", () =>
-      console.log("Database connected")
-    );
-    await mongoose.connect(`${process.env.mongo_url}/screenographix`);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(error.message);
+    console.error("MongoDB connection FAILED:", error.message);
+    process.exit(1); // Stop app if DB fails
   }
 };
+
 export default connectDB;
