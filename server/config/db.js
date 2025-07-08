d643a9d950adc09d4ae3ccf34bd34f4aec8cafd7
Shubha274 <pandeyshubha27@gmail.com>
2025-07-08 07:39:05 +0530

    changes added

--- server/config/db.js
+++ server/config/db.js
@@ -1,29 +1,23 @@
 import mongoose from "mongoose";
-import "dotenv/config";
+import dotenv from "dotenv";
+
+// Load environment variables
+dotenv.config();
+
 const connectDB = async () => {
   try {
-    mongoose.connection.on("connected", () =>
-      console.log("Database connected")
-    );
-    await mongoose.connect(process.env.MONGO_URI);
-  } catch (error) {
-    console.error(error.message);
+    const uri = process.env.MONGO_URI;
+    if (!uri) throw new Error("MONGO_URI is undefined");
+
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("MongoDB Connected");
+  } catch (err) {
+    console.error("MongoDB connection error:", err.message);
   }
 };
-// const connectDB = async () => {
-//   try {
-//     const conn = await mongoose.connect(
-//       `${process.env.MONGO_URI}/screenographix`,
-//       {
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true,
-//       }
-//     );
-//     console.log(`MongoDB Connected: ${conn.connection.host}`);
-//   } catch (error) {
-//     console.error("MongoDB connection FAILED:", error.message);
-//     process.exit(1); // Stop app if DB fails
-//   }
-// };
 
 export default connectDB;
