faa1b7b07382c28e6412e673f74f8562989f73d4
Shubha274 <pandeyshubha27@gmail.com>
2025-07-08 20:57:54 +0530

    changes done

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,15 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:4000", // your backend
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 });
