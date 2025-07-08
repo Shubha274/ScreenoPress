f15a4d759db9b72d87e865925d8e5401a64ae03b
Shubha274 <pandeyshubha27@gmail.com>
2025-07-08 07:18:17 +0530

    changes added

--- server/config/db.js
+++ server/config/db.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
     mongoose.connection.on("connected", () =>
       console.log("Database connected")
     );
-    await mongoose.connect(`${process.env.MONGO_URI}`);
+    await mongoose.connect(process.env.MONGO_URI);
   } catch (error) {
     console.error(error.message);
   }
