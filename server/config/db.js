d574caf064dfde4bffa79f74e88d4a169f8e1f36
Shubha274 <pandeyshubha27@gmail.com>
2025-07-08 07:00:15 +0530

    changes added

--- server/config/db.js
+++ server/config/db.js
@@ -1,28 +1,28 @@
 import mongoose from "mongoose";
-// const connectDB = async () => {
-// try {
-//   mongoose.connection.on("connected", () =>
-//     console.log("Database connected")
-//   );
-//   await mongoose.connect(`${process.env.mongo_url}/screenographix`);
-// } catch (error) {
-//   console.error(error.message);
-// }
-
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(
-      `${process.env.MONGO_URI}/screenographix`,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
+    mongoose.connection.on("connected", () =>
+      console.log("Database connected")
     );
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    await mongoose.connect(`${process.env.MONGO_URI}/screenographix`);
   } catch (error) {
-    console.error("MongoDB connection FAILED:", error.message);
-    process.exit(1); // Stop app if DB fails
+    console.error(error.message);
   }
 };
+// const connectDB = async () => {
+//   try {
+//     const conn = await mongoose.connect(
+//       `${process.env.MONGO_URI}/screenographix`,
+//       {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//       }
+//     );
+//     console.log(`MongoDB Connected: ${conn.connection.host}`);
+//   } catch (error) {
+//     console.error("MongoDB connection FAILED:", error.message);
+//     process.exit(1); // Stop app if DB fails
+//   }
+// };
 
 export default connectDB;
